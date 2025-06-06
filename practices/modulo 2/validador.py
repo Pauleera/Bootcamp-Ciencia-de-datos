a321b6bd5471eb8268d42f43b05c3c74b034ae1c
Pauleera <pfma98@gmail.com>
2025-06-05 23:12:25 -0400

    clase 5 junio

---
+++ practices/modulo 2/validador.py
@@ -0,0 +1,7 @@
+
+respuesta = str(input("¿Cuál es la capital de Chile?"))
+    
+if (respuesta.lower() == "santiago"):
+    print("Correcto, es Santiago")
+else: 
+    print("NOOOO")
---
+++ practices/modulo 2/while_string.py
@@ -0,0 +1,6 @@
+palabra = "paloma"
+i=0
+
+while i < len(palabra):
+    print(f"Letra en posición {i}: {list(palabra)[i]}")
+    i+=1
