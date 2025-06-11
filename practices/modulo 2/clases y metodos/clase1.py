d1569bd07d8f6cec3bfa6d90bad94ee8b5ce63e7
Pauleera <pfma98@gmail.com>
2025-06-10 20:08:16 -0400

    clase 10 junio. Clases y Metodos. add Clase Estudiante

---
+++ practices/modulo 2/clases y metodos/clase1.py
@@ -0,0 +1,13 @@
+
+
+class Estudiante:
+
+    def __init__(self, nombre, matriculado):
+        self.nombre = nombre
+        self.matricula = matriculado
+
+    def presentarse(self):
+        return f"Hola {self.nombre} !"
+
+estudiante1 = Estudiante("Paula", "true")
+print(estudiante1.presentarse())
